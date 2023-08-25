c098c0c19319e989999184c34c552eebe7c6ae10
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-08-25 13:32:43 +0100

    setup all neccessary view controller

---
+++ Spotify/Controllers/Core/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Alexander Nitiola on 25/08/2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController { 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home";
+        view.backgroundColor = UIColor.red;
+    }
+}
+
---
+++ Spotify/Controllers/Core/TabBarViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TabBarViewController.swift
+//  Spotify
+//
+//  Created by Alexander Nitiola on 25/08/2023.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+}
