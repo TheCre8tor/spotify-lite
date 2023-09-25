0c0b5ec6b582e6cfda1bb1aedd4be07e5cca6f5a
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-09-25 16:06:39 +0100

    still configuring the config system

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -7,12 +7,21 @@
 
 import Foundation
 
+struct Constants {
+    static let clientID = ProcessInfo.processInfo.environment["CLIENT_ID"]
+    static let clientSecret = ProcessInfo.processInfo.environment["CLIENT_SECRET"]
+}
+
 final class AuthManager {
     static let shared = AuthManager();
     
     private init() {}
     
     var isSignedIn: Bool {
+        if let userID = ProcessInfo.processInfo.environment["CLIENT_SECRET"] {
+           print("UserID: \(userID)")
+        }
+        
         return false;
     }
     
