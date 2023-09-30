529d33fdd2a06995ecf56e6986369da6c0316629
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-09-30 07:59:33 +0100

    added webView anonymous function and implemented the viewDidLayoutSubviews method for AuthViewController

--- Spotify/Controllers/Other/AuthViewController.swift
+++ Spotify/Controllers/Other/AuthViewController.swift
@@ -5,14 +5,34 @@
 //  Created by Alexander Nitiola on 25/08/2023.
 //
 
-import UIKit
+import UIKit;
+import WebKit;
 
-class AuthViewController: UIViewController {
+class AuthViewController: UIViewController, WKNavigationDelegate {
+    /// This is an anornymous function
+    private let webView: WKWebView = {
+        let prefs = WKWebpagePreferences();
+        prefs.allowsContentJavaScript = true;
+        
+        let config = WKWebViewConfiguration()
+        config.defaultWebpagePreferences = prefs;
+        
+        return WKWebView(frame: .zero, configuration: config);
+    }()
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        super.viewDidLoad();
+        
+        title = "Sign In";
+        view.backgroundColor = .systemBackground;
+        webView.navigationDelegate = self;
+        view.addSubview(webView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews();
+        
+        webView.frame = view.bounds;
     }
     
 
